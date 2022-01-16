d8d4a5c2bf419c9dd114f04d3c1c7493aa9cdde3
theCyberTech <84775494+theCyberTech@users.noreply.github.com>
2022-01-16 18:00:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UFC-Event-Scraper
+ A python based scraping tool to retrieve all the current upcoming UFC events
---
+++ ufc_scraper.py
@@ -0,0 +1,33 @@
+# Name : ufc_scraper.py v0.0.1
+# Author : thCyberTech
+# Date : 12th Jan 2022
+# Description : A class based python file that scrapes the UFC website for the latest events
+
+import requests
+from bs4 import BeautifulSoup
+
+class Scraper:
+
+    def __init__(self):
+        self.url = "https://www.ufc.com/events"
+        self.page = requests.get(self.url)
+        self.soup = BeautifulSoup(self.page.content, "html.parser")
+        self.results = self.soup.find(id="events-list-upcoming")
+        self.upcoming_events = self.results.find_all("div", class_="c-card-event--result__info")
+        self.event = None
+        self.fight = None
+        self.fight_time_main = None
+        self.fight_location = None
+
+    def get_tags(self):
+        # print(self.soup.title.text)
+        for self.event in self.upcoming_events:
+            self.fight = self.event.find("h3", class_="c-card-event--result__headline")
+            self.fight_time_main = self.event.find("div", class_="c-card-event--result__date tz-change-data")
+            self.fight_location = self.event.find("div", class_="field field--name-taxonomy-term-title field--type-ds field--label-hidden field__item")
+            print(self.fight.text.strip())
+            print(self.fight_time_main.text.strip())
+            print(self.fight_location.text.strip())
+
+s = Scraper()
+s.get_tags()
